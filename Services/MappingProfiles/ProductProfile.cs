3f3c213769ddda95fa5876e9cbebd63bf4b0f09e
Marwan <marwan-walid1@hotmail.com>
2025-04-17 18:22:21 +0200

    Video 6

--- Services/MappingProfiles/ProductProfile.cs
+++ Services/MappingProfiles/ProductProfile.cs
@@ -8,13 +8,23 @@ public class ProductProfile : Profile
     {
         CreateMap<Product, ProductResponse>()
             .ForMember(d => d.BrandName, options
-                => options.MapFrom(s => s.ProductBrand.Name));
-
-        CreateMap<Product, ProductResponse>()
+                => options.MapFrom(s => s.ProductBrand.Name))
             .ForMember(d => d.TypeName, options
-                => options.MapFrom(s => s.ProductType.Name));
+                => options.MapFrom(s => s.ProductType.Name))
+            .ForMember(d => d.PictureUrl,
+                options =>
+                    options.MapFrom<PictureUrlResolver>()) ;
 
         CreateMap<ProductBrand, BrandResponse>();
         CreateMap<ProductType, TypeResponse>();
+    }
+}
+
+public class PictureUrlResolver : IValueResolver<Product, ProductResponse, string>
+{
+    public string Resolve(Product source, ProductResponse destination, string destMember, ResolutionContext context)
+    {
+        if (string.IsNullOrEmpty(source.PictureUrl)) return "";
+        return $"http://localhost:5000/{source.PictureUrl}" ;
     }
 }
